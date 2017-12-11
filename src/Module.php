26bbcb4821401323c8279e0e52017a40c3f824d7
Pavel Topol <khai603@gmail.com>
2017-12-11 12:20:41 +0200

    rm Module propperties

--- src/Module.php
+++ src/Module.php
@@ -7,10 +7,4 @@ class Module extends \yii\base\Module
 
     public $controllerNamespace = 'klisl\languages\controllers';
 
-    public $languages; //Языки используемые в приложении
-
-    public $default_language; //основной язык (по-умолчанию)
-
-    public $show_default; //показывать в URL основной язык
-
 }
